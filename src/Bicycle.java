74f3a9a0de0ed7ae6be67353190790bab7f0b956
unknown <BT_3W2_02@BKTECH.LOCAL>
2023-03-28 09:24:34 -0400

    Initial commit

---
+++ src/Bicycle.java
@@ -0,0 +1,16 @@
+public class Bicycle extends Vehicle
+{
+    private int gears;
+
+    public Bicycle(String name, int wheels, int gears)
+    {
+        super(name,wheels);
+        this.gears = gears;
+    }
+    public int getGears(){
+        return gears;
+    }
+    public void ringBell(){
+        System.out.println("Ring ring, bike here");
+    }
+}
